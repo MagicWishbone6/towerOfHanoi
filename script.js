b8dd0006c6575d178410be48d743c68916c543ec
magicwishbone6 <caitlin.cmo@gmail.com>
2020-12-17 16:07:29 -0600

    can highlight brick on selection

--- script.js
+++ script.js
@@ -12,3 +12,7 @@ let brick1 = document.querySelector("#brick1")
 let brick2 = document.querySelector("#brick2")
 let brick3 = document.querySelector("#brick3")
 
+// Highlight a brick on click
+brick1.addEventListener("click", () => brick1.style.borderColor = "orange")
+brick2.addEventListener("click", () => brick1.style.borderColor = "orange")
+brick3.addEventListener("click", () => brick1.style.borderColor = "orange")
