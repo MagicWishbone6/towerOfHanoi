8de5f8ff6bd3c75de7430df6b6406d369cd730e5
magicwishbone6 <caitlin.cmo@gmail.com>
2020-12-17 16:01:52 -0600

    basic CSS done, towers and bricks in js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("test")
+
+/* TOWER OF HANOI GAME */
+
+///// Towers
+let tower1 = document.querySelector("#tower1")
+let tower2 = document.querySelector("#tower2")
+let tower3 = document.querySelector("#tower3")
+
+///// Bricks
+let brick1 = document.querySelector("#brick1")
+let brick2 = document.querySelector("#brick2")
+let brick3 = document.querySelector("#brick3")
+
