8f324dcc349f3e0cb9916c1d1341b49ca46c1c5b
magicwishbone6 <caitlin.cmo@gmail.com>
2020-12-17 17:43:25 -0600

    selections highlight on a click toggle

--- script.js
+++ script.js
@@ -2,17 +2,36 @@ console.log("test")
 
 /* TOWER OF HANOI GAME */
 
-///// Towers
+///// Towers /////
 let tower1 = document.querySelector("#tower1")
 let tower2 = document.querySelector("#tower2")
 let tower3 = document.querySelector("#tower3")
 
-///// Bricks
+///// Bricks /////
 let brick1 = document.querySelector("#brick1")
 let brick2 = document.querySelector("#brick2")
 let brick3 = document.querySelector("#brick3")
 
-// Highlight a brick on click
-brick1.addEventListener("click", () => brick1.style.borderColor = "orange")
-brick2.addEventListener("click", () => brick1.style.borderColor = "orange")
-brick3.addEventListener("click", () => brick1.style.borderColor = "orange")
+// Array of all Bricks
+let bricks = [brick1, brick2, brick3]
+
+// Each click on any brick will toggle boolean selectionPresent
+let selectionPresent = false
+
+// Highlight 1 brick on click
+// Turn highlight OFF by clicking ANY brick
+function selectBrick() {
+    if (selectionPresent === true) {
+        bricks.forEach((element) => element.style.borderColor = "crimson")
+        selectionPresent = false
+    } else {
+        this.style.borderColor = "orange"
+        selectionPresent = true
+    }
+    console.log(selectionPresent)
+}
+
+brick1.addEventListener("click", selectBrick)
+brick2.addEventListener("click", selectBrick)
+brick3.addEventListener("click", selectBrick)
+
